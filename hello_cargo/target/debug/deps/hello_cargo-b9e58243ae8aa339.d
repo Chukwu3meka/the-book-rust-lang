da87f47c6e161efeaa2383af1ec2236294b87e8e
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-07-04 21:38:37 +0100

    create new project with cargo new

---
+++ hello_cargo/target/debug/deps/hello_cargo-b9e58243ae8aa339.d
@@ -0,0 +1,5 @@
+E:\software\Rust\the_book\hello_cargo\target\debug\deps\hello_cargo-b9e58243ae8aa339.rmeta: src\main.rs
+
+E:\software\Rust\the_book\hello_cargo\target\debug\deps\hello_cargo-b9e58243ae8aa339.d: src\main.rs
+
+src\main.rs:
