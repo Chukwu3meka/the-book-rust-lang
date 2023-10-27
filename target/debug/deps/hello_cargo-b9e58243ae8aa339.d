a37133904a3fe2e0bcd3ec17775c8e67e6b891d6
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-10-27 20:56:49 +0100

    add executable and gitignore

--- target/debug/deps/hello_cargo-b9e58243ae8aa339.d
+++
@@ -1,5 +0,0 @@
-E:\Rust\the_book_rust_lang\target\debug\deps\hello_cargo-b9e58243ae8aa339.rmeta: src\main.rs
-
-E:\Rust\the_book_rust_lang\target\debug\deps\hello_cargo-b9e58243ae8aa339.d: src\main.rs
-
-src\main.rs:
--- target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-E:\software\Rust\the_book\target\debug\hello_cargo.exe: E:\software\Rust\the_book\src\main.rs
